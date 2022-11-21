31eb44beb4b2257654de7e03094f79d7ec64ef0e
Benjamin  Déséveaux <44895614+Spourix@users.noreply.github.com>
2022-11-21 19:52:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import win32api
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,15 +8,13 @@ app = Flask(__name__)
 #0x00001000 - This makes the popup appear over the browser window
 @app.route('/')
 def index():
-    win32api.MessageBox(0, 'You have just run a python script on the page load!', 'Running a Python Script via Javascript', 0x00001000)
-    return render_template('index.html')
+    return render_template('hello ça marche')
 
 #Using the below, the popup message appears when the button is clicked on the webpage.
 #0x00001000 - This makes the popup appear over the browser window
 @app.route('/test')
 def test():
-    win32api.MessageBox(0, 'You have just run a python script on the button press!', 'Running a Python Script via Javascript', 0x00001000)
-    return render_template('index.html')
+    return render_template('hello ça marche')
 
 if __name__ == "__main__":
     app.run(debug=True)
